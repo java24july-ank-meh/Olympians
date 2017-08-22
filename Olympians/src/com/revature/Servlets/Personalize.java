a5f14c8221d23e7cc116c919b262842fcfd85d7b
jearthurs <juliaarthurs@gmail.com>
2017-08-21 23:07:39 -0400

    User Stories 11-20 Servlets

---
+++ Olympians/src/com/revature/Servlets/Personalize.java
@@ -0,0 +1,40 @@
+package com.revature.Servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class Personalize extends HttpServlet {
+	
+	/*
+	 * use to personalize background colors/ image
+	 * use to set background music if possible
+	 */
+	private static final long serialVersionUID = 1L;
+       
+   
+    public Personalize() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
