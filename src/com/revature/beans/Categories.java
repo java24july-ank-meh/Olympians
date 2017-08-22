e047c7fb02b171c86e1c77ed65c6e5bf33865c13
erosales3 <eleazar.rosales@gmail.com>
2017-08-22 13:16:28 -0400

    The POJOs! No other POJO will ever exist. Unless the project requires them.

---
+++ src/com/revature/beans/Categories.java
@@ -0,0 +1,28 @@
+package com.revature.beans;
+
+public class Categories {
+
+	private int cid;
+	private String cname;
+	public int getCid() {
+		return cid;
+	}
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public Categories(int cid, String cname) {
+		super();
+		this.cid = cid;
+		this.cname = cname;
+	}
+	public Categories() {
+		super();
+	}
+	
+}
